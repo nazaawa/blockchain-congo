5817b840c13c699ae9b93c1322a46f7d38c777c2
Johnny Andedi <64134324+nazaawa@users.noreply.github.com>
2022-12-27 22:43:02 +0100

    feat hashblock

--- dev/test.js
+++ dev/test.js
@@ -3,5 +3,7 @@ const bitcoin =  new Blockchain();
 bitcoin.createNewBloc(234,"erer","ererre")
 bitcoin.createNewTransactions(214,"johny","alex")
 bitcoin.createNewBloc(234,"erdffer","erzererre")
+bitcoin.createNewTransactions(214,"johny","alex")
+bitcoin.createNewBloc(234,"erdffer","erzererre")
 
 console.log(bitcoin.chain[1])
