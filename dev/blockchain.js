2c88beceb9ec3eef102eb888a21a146722b9c603
Johnny Andedi <64134324+nazaawa@users.noreply.github.com>
2023-08-19 19:09:31 +0100

    block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,17 +1,18 @@
-const sha256 = requrie("sha256")
+const sha256 = require("sha256")
 
 function Blockchain (){
     this.chain = [];
     this.pendingTransactions = [];
+    this.createNewBloc(100,"0","0");
 
 }
 
-Blockchain.prototype.createNewBloc =  function (nounce, previousBlocHash , hash){
+Blockchain.prototype.createNewBloc =  function (nonce, previousBlocHash , hash){
     const newBlock = {
         index :this.chain.length + 1,
         timestamp : Date.now(),
         transaction : this.pendingTransactions,
-        nounce : nounce ,
+        nonce : nonce ,
         hash : hash,
         previousBlocHash : previousBlocHash,
     }
@@ -20,7 +21,7 @@ Blockchain.prototype.createNewBloc =  function (nounce, previousBlocHash , hash)
     return newBlock;
 }
 
-Blockchain.prototype.getlastBlock = function(){
+Blockchain.prototype.getLastBlock = function(){
     return this.chain[this.chain.length - 1];
 }
 
@@ -28,14 +29,25 @@ Blockchain.prototype.createNewTransactions = function(amount,sender ,recipient){
     const newTransaction = { amount : amount, sender : sender,recipient : recipient};
     this.pendingTransactions.push(newTransaction);
 
-    return this.getlastBlock()['index'] + 1;
+    return this.getLastBlock()['index'] + 1;
 }
 
-Blockchain.prototype.hashBlock = function(previousBlocHash, currentBlockData , nounces) {
-    const dataAsString = previousBlocHash +  nounces.toString() +  JSON.stringify(currentBlockData);
+Blockchain.prototype.hashBlock = function(previousBlocHash, currentBlockData , nonces) {
+    const dataAsString = previousBlocHash +  nonces.toString() +  JSON.stringify(currentBlockData);
     const hash = sha256(dataAsString)
     return hash;
 }
 
+Blockchain.prototype.proofOfProof = function (previousBlocHash , currentBlockData) {
+let nonce =  0;
+let hash = this.hashBlock(previousBlocHash, currentBlockData,nonce)
+
+while (hash.substring(0,4) !== '00000'){
+    nonce++;
+    hash = this.hashBlock(previousBlocHash, currentBlockData,nonce)
+    console.log(hash)
+}
+    return nonce;
+}
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,9 +1,5 @@
 const Blockchain =  require("./blockchain")
 const bitcoin =  new Blockchain();
-bitcoin.createNewBloc(234,"erer","ererre")
-bitcoin.createNewTransactions(214,"johny","alex")
-bitcoin.createNewBloc(234,"erdffer","erzererre")
-bitcoin.createNewTransactions(214,"johny","alex")
-bitcoin.createNewBloc(234,"erdffer","erzererre")
 
-console.log(bitcoin.chain[1])
+
+console.log(bitcoin);
